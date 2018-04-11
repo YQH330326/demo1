5cb219ad160a39b03eda58834bbcf106e1b7285d
YQH330326 <754804556@qq.com>
2018-04-11 21:05:24 +0800

    第一次玩这个啊

---
+++ src/main/java/com/qf/service/impl/StudentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.qf.service.impl;
+
+/**
+ * Created by asus on 2018/4/11.
+ */
+public class StudentServiceImpl {
+    public static  void main(String[] args){
+        System.out.print("yes");
+        System.out.print("no");
+    }
+}
