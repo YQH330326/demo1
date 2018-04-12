bcc3d5dedefcbb264024649edd08510fd56bc6ba
YQH330326 <754804556@qq.com>
2018-04-12 17:41:43 +0800

    第一次玩这个啊

--- src/main/java/com/qf/service/impl/StudentServiceImpl.java
+++ src/main/java/com/qf/service/impl/StudentServiceImpl.java
@@ -5,7 +5,7 @@ package com.qf.service.impl;
  */
 public class StudentServiceImpl {
     public static  void main(String[] args){
-        System.out.print("yes");
-        
+        System.out.print("yes");;;
+
     }
 }
